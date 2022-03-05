9b4289e0cf7f2eeaa18b8c3dd3ab19e8df71ca6d
mxchristian17 <90223948+mxchristian17@users.noreply.github.com>
2022-03-05 20:59:34 -0300

    services icons added

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -18,22 +18,27 @@ const Services = () => {
             </div>
             <div className="row p-2">
                 <div className="col-12 col-lg">
+                    <img src="/Images/Services/wedding-rings.png" width="80" alt="weddings" />
                     <h4 className="pb-3" style={{'fontWeight' : '600'}}>Ceremonia de casamiento</h4>
                     <p style={manropeFont} className="text-center lh-lg fw-light">Una selección de obras musicales especialmente curada para acompañar uno de los momentos más importantes de nuestras vidas. También podemos incluir “esa canción especial” que los une.</p>
                 </div>
                 <div className="col-12 col-lg">
+                    <img src="/Images/Services/cocktail-toast.png" width="80" alt="cocktails" />
                     <h4 className="pb-3" style={{'fontWeight' : '600'}}>Cocktail - Recepción</h4>
                     <p style={manropeFont} className="text-center lh-lg fw-light">Música especialmente seleccionada para amenizar el momento social, de diversos estilos, desde los clásicos a lo moderno.</p>
                 </div>
                 <div className="col-12 col-lg">
+                    <img src="/Images/Services/private-events.png" width="80" alt="events" />
                     <h4 className="pb-3" style={{'fontWeight' : '600'}}>Eventos privados</h4>
                     <p style={manropeFont} className="text-center lh-lg fw-light">Adaptamos nuestro servicio musical a tus necesidades. Creamos experiencias musicales que acompañan y realzan el evento. Podemos musicalizar desde diferentes estilos, incluso interpretar música institucional o compuesta especialmente para dicho acontecimiento.</p>
                 </div>
                 <div className="col-12 col-lg">
+                    <img src="/Images/Services/private-concerts.png" width="80" alt="concerts" />
                     <h4 className="pb-3" style={{'fontWeight' : '600'}}>Concierto íntimo de música clásica</h4>
                     <p style={manropeFont} className="text-center lh-lg fw-light">Concierto exclusivo de obras compuestas por los más grandes compositores de la música clásica para dúo de violín y viola.</p>
                 </div>
                 <div className="col-12 col-lg">
+                    <img src="/Images/Services/more-services.png" width="80" alt="moreServices" />
                     <h4 className="pb-3" style={{'fontWeight' : '600'}}>Más Servicios</h4>
                     <p style={manropeFont} className="text-center lh-lg fw-light">Conciertos de trío, cuarteto y/o ensamble de cuerdas. Consultanos desde la sección de Contacto.</p>
                 </div>
